b658e93a1ac4fa26030e8ceabf0453ca3b050694
NandaReisVerissimo <nandareis83@gmail.com>
2022-08-29 13:10:13 -0300

    tentativa desafio html

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-const inputTarefa = document.getElementById ("tarefa-nova")
+let idTarefa = 0
+const inputTarefa = document.getElementById("tarefa-nova")
 
-function adicionarTarefa () {
-    const item = document.createElement ("li");
+function adicionarTarefa() {
+ 
+    const item = document.createElement("li");
+    idTarefa++;
+    item.id = idTarefa 
 
-    item.innerHTML = `<li>
-        span>${inputTarefa.value}</span>
-        <button class="excluir" >X</button>
-</li>`;
+    item.innerHTML = `
+        <span onclick="concluir(${idTarefa})">${inputTarefa.value}</span>
+        <button class="excluir" onclick="excluir(event)">x</button>`;
 
-    const lista = document.querySelector ("#lista");
-    lista.appendChild (item);
+       const lista = document.querySelector("#lista");
+    lista.appendChild(item);
 
     inputTarefa.value = ""
 }
 
-const botaoAdicionar = document.querySelector ("#btn-adicionar")
-botaoAdicionar.addEventListener ("click", adicionarTarefa)
+const botaoAdicionar = document.querySelector("#btn-adicionar")
+botaoAdicionar.addEventListener("click", adicionarTarefa)
 
-function limparLista {
-    const lista = document.querySelector ("#lista");
+function limparLista() {
+    const lista = document.querySelector("#lista");
     lista.innerHTML = ""
 }
-const botaoLimpar = document.querySelector (".bt-azul")
+const botaoLimpar = document.querySelector(".bt-azul")
 
-botaoLimpar.addEventListener ("click", limparLista)
+botaoLimpar.addEventListener("click", limparLista)
+
+function concluir (itemId) {
+    const tarefaSelecionada = document.getElementById (itemId);
+    tarefaSelecionada.className = "concluida";
+}
+
+function excluir (itemId) {
+    const tarefaSelecionada = document.getElementById (itemId);
+    tarefaSelecionada.className = "excluir"
+    tarefaSelecionada.remove ()
+}
+
+const botaoExcluir = document.querySelector (".excluir")
+botaoExcluir.addEventListener ("click", excluir)
 
 
